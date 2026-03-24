b5decb42113d86423e7e3ad36adc888aee4b96c0
Rizaly <aranezrizaly949@gmail.com>
2026-03-24 12:14:50 +0800

    feat:implement product MVC structure

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,6 @@
+<h1>Product List</h1>
+
+@foreach($products as $product)
+<p>{{$product->name }} - {{$product->price }} </p>
+
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+Route::get('/products', [ProductController::class, 'index']);
